9f3380fead8b23b2b09dbc3faa8b838efddb6915
Estrangin <etienne.estrangin@gmail.com>
2025-01-06 09:24:03 +0100

    P2CH2: animation for the departure or the arrival of an element

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -25,6 +25,18 @@ import { Comment } from 'src/app/core/models/comment.model';
             transition('active => default', [
                 animate('500ms ease-in-out')
             ]),
+            transition('void => *', [
+                style({
+                    transform: 'translateX(-100%)',
+                    opacity: 0,
+                    'background-color': 'rgb(201, 157, 242)',
+                }),
+                animate('250ms ease-out', style({
+                    transform: 'translateX(0)',
+                    opacity: 1,
+                    'background-color': 'white',
+                }))
+            ])
         ])
     ]
 })
@@ -50,6 +62,13 @@ export class CommentsComponent implements OnInit {
         if (this.commentCtrl.invalid) {
             return;
         }
+        const maxId = Math.max(...this.comments.map(comment => comment.id));
+        this.comments.unshift({
+            id: maxId + 1,
+            comment: this.commentCtrl.value,
+            createdDate: new Date().toISOString(),
+            userId: 1
+        });
         this.newComment.emit(this.commentCtrl.value);
         this.commentCtrl.reset();
     }
@@ -57,7 +76,7 @@ export class CommentsComponent implements OnInit {
     onListItemMouseEnter(index: number) {
         this.animationStates[index] = 'active';
     }
-    
+
     onListItemMouseLeave(index: number) {
         this.animationStates[index] = 'default';
     }
